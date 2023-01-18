f1f3527ffcbce750714db3f2978c7d69ddeac02e
RuslanDidun <ruslandidun5@gmail.com>
2023-01-17 22:06:59 -0700

    from styles, app styles

---
+++ client/src/components/Posts/Post/Post.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Post from './Post/Post'
+
+
+const Post = () => {
+  return (
+    <>
+      <h1>Posts</h1>
+      <Post />
+      <Post />
+    </>
+  )
+}
+
+export default Post;
---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const Posts = () => {
+  return(
+    <h1>Posts</h1>
+  )
+}
+
+export default Posts;
