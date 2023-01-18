cf1890c6d9f03de18a98194497d947a0ad4a4d0b
RuslanDidun <ruslandidun5@gmail.com>
2023-01-18 11:00:38 -0700

    styles, reducers, store

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import {createStore, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
+import reducers from './reducers'
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)))
 
 ReactDom.render(<App />, document.getElementById('root'));
