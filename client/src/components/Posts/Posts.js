381e8557a1791905cee66cb42146c5f021e548a3
RuslanDidun <ruslandidun5@gmail.com>
2023-01-22 12:48:15 -0700

    update post

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -6,7 +6,7 @@ import { Grid, CircularProgress } from '@material-ui/core'
 
 
 
-const Posts = () => {
+const Posts = ({setCurrentId}) => {
 
   const posts = useSelector((state) => state.posts)
   const classes = useStyles();
@@ -17,7 +17,7 @@ const Posts = () => {
       <Grid className={classes.container} container alignItems='stretch' spacing={3}>
         {posts.map((post) => (
           <Grid key={post._id} item sx={12} sm={6}>
-            <Post post={post} />
+            <Post post={post} setCurrentId={setCurrentId}/>
           </Grid>
         ))}
       </Grid>
