fc0c230bfbb04fa7a01382c6c4d1b10017409c3b
RuslanDidun <ruslandidun5@gmail.com>
2023-01-23 18:15:40 -0700

    like post

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -37,3 +37,12 @@ export const deletePost = (id) => async (dispatch) => {
   }
 }
 
+export const likePost = (id) => async (dispatch) => {
+  try {
+    const { data } = await api.likePost(id);
+    dispatch({ type: 'LIKE', payload: data })
+  } catch (error) {
+    console.log(error);
+  }
+}
+
--- client/src/components/Posts/Post/Post.jsx
+++ client/src/components/Posts/Post/Post.jsx
@@ -13,7 +13,7 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
 import { useDispatch } from 'react-redux';
-import { deletePost } from '../../../actions/posts';
+import { deletePost, likePost } from '../../../actions/posts';
 
 
 
@@ -49,7 +49,7 @@ const Post = ({ post, setCurrentId }) => {
         <Typography variant='h5' gutterBottom>{post.message}</Typography>
       </CardContent>
       <CardActions className={classes.cardActions}>
-        <Button size='small' color='primary' onClick={() => { }}>
+        <Button size='small' color='primary' onClick={() => dispatch(likePost(post._id))}>
           <ThumbUpAltIcon fontSize='small' /> Like {post.likeCount}
         </Button>
         <Button size='small' color='primary' onClick={() => dispatch(deletePost(post._id))}>
