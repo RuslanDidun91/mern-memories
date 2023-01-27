13dea2f2dc8ce9caf5f62939b525ffff8ff98800
RuslanDidun <ruslandidun5@gmail.com>
2023-01-27 09:29:43 -0700

    signUp, signIn

---
+++ server/models/users.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+
+const postSchema = mongoose.Schema({
+  name: {type: String, required: true},
+  email: {type: String, required: true},
+  password: {type: String, required: true},
+  id: {type: String},
+})
+
+//turn schema into a model
+export default mongoose.model('User', userSchema);
